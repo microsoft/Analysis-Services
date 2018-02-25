f39f48e269f6c2f963a54266d87b5bc940db593f
Christian Wade <bism.normalizer@gmail.com>
2018-02-25 09:05:51 -0800

    Newtonsoft embedded

--- BismNormalizer/BismNormalizer/Properties/AssemblyInfo.cs
+++ BismNormalizer/BismNormalizer/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.22")]
-[assembly: AssemblyFileVersion("4.0.0.22")]
+[assembly: AssemblyVersion("4.0.0.25")]
+[assembly: AssemblyFileVersion("4.0.0.25")]
