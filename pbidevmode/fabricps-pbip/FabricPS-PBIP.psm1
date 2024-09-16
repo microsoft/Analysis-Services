0c761d74f2af0bc4b49e4dabf0bce63456f72736
Michaël Rioux <152207505+JANA-mrioux@users.noreply.github.com>
2024-09-16 14:07:18 -0400

    Update FabricPS-PBIP.psm1
    To work under ubuntu machine (linux)

--- pbidevmode/fabricps-pbip/FabricPS-PBIP.psm1
+++ pbidevmode/fabricps-pbip/FabricPS-PBIP.psm1
@@ -753,7 +753,7 @@ Function Import-FabricItems {
                 }
             }
 
-            $partPath = $filePath.Replace($itemPathAbs, "").TrimStart("\").Replace("\", "/")
+            $partPath = $filePath.Replace($itemPathAbs, "").TrimStart("\").TrimStart("/").Replace("\", "/")
 
             $fileEncodedContent = ($fileContent) ? [Convert]::ToBase64String($fileContent) : ""
             
@@ -979,7 +979,7 @@ Function Import-FabricItem {
             $fileContent = Get-Content -LiteralPath $filePath -AsByteStream -Raw
         }
         
-        $partPath = $filePath.Replace($itemPathAbs, "").TrimStart("\").Replace("\", "/")
+        $partPath = $filePath.Replace($itemPathAbs, "").TrimStart("\").TrimStart("/").Replace("\", "/")
 
         $fileEncodedContent = ($fileContent) ? [Convert]::ToBase64String($fileContent) : ""
         
